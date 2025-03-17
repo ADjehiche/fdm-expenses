9bcd721cd18f7cd6a706725197cf8a3fa00bd417
areon546 <57325743+areon546@users.noreply.github.com>
2025-03-17 10:08:57 +0000

    Add login screen, no functionality

---
+++ app/(logged-in-screens)/admin/page.tsx
@@ -0,0 +1,17 @@
+'use server'
+
+import Image from "next/image";
+import Form from "next/form";
+
+
+
+
+export default async function Home() {
+  return (
+    <div className="">
+      <div className="">
+        <p>Hi firends</p>
+      </div>
+    </div>
+  );
+}
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,30 @@
+import Form from "next/form";
+
+import {register} from "@/actions/register";
+
+
+
+export default async function registerForm() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="">
+
+        <Form action={register}>
+          {/* username */}
+          <label id="uname">Username:</label><br/>
+          <input name="uname" type="text" className="border"/>
+
+          <br />
+          {/* password */}
+
+          <label id="pass">Password:</label><br/>
+          <input name="pass" className="border"/>
+
+          {/* submit button */}
+          <br />
+          <button type="submit">Submit</button>
+        </Form>
+      </div>
+    </div>
+  );
+}
