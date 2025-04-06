45a6024247a1641643ad63022d1e474eac0fd43c
Acil <acil.djehiche@outlook.com>
2025-04-06 19:41:34 +0100

    Moved the admin button to the side menu of the dashboard, changed the form to be more like the login form and used zod to resolve it

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -39,13 +39,6 @@ export default function dashboardPage() {
   return (
     <div className="space-y-6 text-black">
 
-      <Button asChild className="bg-[#c3fa04] hover:bg-[#c3fa04]/90">
-        <Link href="/dashboard/admin">
-          <Plus className="mr-2 h-4 2-4" />
-          Administrator View
-        </Link>
-      </Button>
-      
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold tracking-tight">Dashboard</h1>
         <Button asChild className="bg-[#c3fa04] hover:bg-[#c3fa04]/90">
