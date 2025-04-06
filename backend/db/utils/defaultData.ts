fd28714062979b96a10ab159640b92810f0aac14
areon546 <57325743+areon546@users.noreply.github.com>
2025-04-06 19:36:42 +0100

    add basic claim methods

--- backend/db/utils/defaultData.ts
+++ backend/db/utils/defaultData.ts
@@ -2,8 +2,19 @@ import { DatabaseManager } from "../databaseManager";
 import { Administrator } from "@/backend/employee/administrator";
 import { LineManager } from "@/backend/employee/lineManager";
 import { PayrollOfficer } from "@/backend/employee/payrollOfficer";
-import { createAdmin, createConsultant, createGeneralEmployee, createLineManager, createPayroll } from "@/backend/db/utils/helpers"
-
+import {
+  createAdmin,
+  createApprovedClaim,
+  createConsultant,
+  createDraftClaim,
+  createGeneralEmployee,
+  createLineManager,
+  createPayroll,
+  createPendingClaim,
+  createRejectedClaim,
+} from "@/backend/db/utils/helpers";
+import { Claim, ClaimStatus } from "@/backend/claims/claim";
+import { User } from "@/backend/user";
 
 async function InsertDefaultData() {
   const db = DatabaseManager.getInstance();
@@ -25,103 +36,17 @@ async function InsertDefaultData() {
   adminRole.setEmployeesLineManager(general.getId(), lineManager.getId());
   adminRole.setEmployeesLineManager(consultant.getId(), lineManager.getId());
 
-  // Create a draft claim for the general employee
+  // Create a draft claim
+  await createDraftClaim(general);
 
-  const draftClaim = await general.getEmployeeRole().createDraftClaim();
-  if (!draftClaim) {
-    throw new Error("General Staff Claim Insert failed");
-  }
-  console.log(
-    "General Staff Draft Claim Insert successful",
-    `id: ${draftClaim.getId()}`
-  );
-
-  // Create a submitted claim for the consultant employee
-
-  const generalEmployeeClaim = await general
-    .getEmployeeRole()
-    .createDraftClaim();
-  if (!generalEmployeeClaim) {
-    throw new Error("General Staff Pending Claim Insert failed");
-  }
-  console.log(
-    "General Staff Pending Claim Insert successful",
-    `id: ${generalEmployeeClaim.getId()}`
-  );
-
-  const updatedGeneralEmployeeClaim = await general
-    .getEmployeeRole()
-    .updateClaimAmount(generalEmployeeClaim, 100);
-  if (!updatedGeneralEmployeeClaim) {
-    throw new Error("General Staff Pending Claim Update failed");
-  }
-  console.log(
-    "General Staff Pending Claim Update successful",
-    `id: ${updatedGeneralEmployeeClaim.getId()}, amount: ${updatedGeneralEmployeeClaim.getAmount()}`
-  );
-
-  const submittedGeneralEmployeeClaim = await general
-    .getEmployeeRole()
-    .submitClaim(updatedGeneralEmployeeClaim);
-  if (!submittedGeneralEmployeeClaim) {
-    throw new Error("General Staff Pending Claim Submit failed");
-  }
-  console.log(
-    "General Staff Pending Claim Submit successful",
-    `id: ${submittedGeneralEmployeeClaim.getId()}, amount: ${submittedGeneralEmployeeClaim.getAmount()} status: ${submittedGeneralEmployeeClaim.getStatus()}`
-  );
+  // Create a pending claim
+  await createPendingClaim(general, 100);
 
   // Create a accepted claim
+  await createApprovedClaim(general, 500, lineManager);
 
-  const acceptedEmployeeClaim = await general
-    .getEmployeeRole()
-    .createDraftClaim();
-  if (!acceptedEmployeeClaim) {
-    throw new Error("General Staff Accepted Claim Insert failed");
-  }
-  console.log(
-    "General Staff Accepted Claim Insert successful",
-    `id: ${acceptedEmployeeClaim.getId()}`
-  );
-
-  const updatedAcceptedEmployeeClaim = await general
-    .getEmployeeRole()
-    .updateClaimAmount(acceptedEmployeeClaim, 500);
-  if (!updatedAcceptedEmployeeClaim) {
-    throw new Error("General Staff Accepted Claim Update failed");
-  }
-  console.log(
-    "General Staff Accepted Claim Update successful",
-    `id: ${updatedAcceptedEmployeeClaim.getId()}, amount: ${updatedAcceptedEmployeeClaim.getAmount()}`
-  );
-
-  const acceptedEmployeeClaimSubmit = await general
-    .getEmployeeRole()
-    .submitClaim(updatedAcceptedEmployeeClaim);
-  if (!acceptedEmployeeClaimSubmit) {
-    throw new Error("General Staff Accepted Claim Submit failed");
-  }
-  console.log(
-    "General Staff Accepted Claim Submit successful",
-    `id: ${acceptedEmployeeClaimSubmit.getId()}, amount: ${acceptedEmployeeClaimSubmit.getAmount()} status: ${acceptedEmployeeClaimSubmit.getStatus()}`
-  );
-
-  const lineManagerClaims = await lineManagerRole.getEmployeeSubmittedClaims();
-  if (lineManagerClaims.length !== 2) {
-    throw new Error(
-      `Line Manager Claims Insert failed length: ${lineManagerClaims.length}`
-    );
-  }
-  const acceptedClaim = await lineManagerRole.approveClaim(
-    acceptedEmployeeClaimSubmit
-  );
-  if (!acceptedClaim) {
-    throw new Error("Line Manager Accepted Claim Insert failed");
-  }
-  console.log(
-    "Line Manager Accepted Claim Insert successful",
-    `id: ${acceptedClaim.getId()}, amount: ${acceptedClaim.getAmount()} status: ${acceptedClaim.getStatus()}`
-  );
+  // Create a rejected claim
+  await createRejectedClaim(general, 1500, lineManager);
 
   // Create a reimbursed claim
 
@@ -182,52 +107,6 @@ async function InsertDefaultData() {
   console.log(
     "Payroll Officer Claim Insert successful",
     `id: ${payrollOfficerClaims.getId()}, amount: ${payrollOfficerClaims.getAmount()} status: ${payrollOfficerClaims.getStatus()}`
-  );
-
-  // Create a rejected claim
-
-  const rejectedClaim = await general.getEmployeeRole().createDraftClaim();
-  if (!rejectedClaim) {
-    throw new Error("General Staff Rejected Claim Insert failed");
-  }
-  console.log(
-    "General Staff Rejected Claim Insert successful",
-    `id: ${rejectedClaim.getId()}`
-  );
-  const updatedRejectedClaim = await general
-    .getEmployeeRole()
-    .updateClaimAmount(rejectedClaim, 1500);
-  if (!updatedRejectedClaim) {
-    throw new Error("General Staff Rejected Claim Update failed");
-  }
-  console.log(
-    "General Staff Rejected Claim Update successful",
-    `id: ${updatedRejectedClaim.getId()}, amount: ${updatedRejectedClaim.getAmount()}`
-  );
-  const rejectedClaimSubmit = await general
-    .getEmployeeRole()
-    .submitClaim(updatedRejectedClaim);
-  if (!rejectedClaimSubmit) {
-    throw new Error("General Staff Rejected Claim Submit failed");
-  }
-  console.log(
-    "General Staff Rejected Claim Submit successful",
-    `id: ${rejectedClaimSubmit.getId()}, amount: ${rejectedClaimSubmit.getAmount()} status: ${rejectedClaimSubmit.getStatus()}`
-  );
-  const lineManagerClaims3 = await lineManagerRole.getEmployeeSubmittedClaims();
-  if (lineManagerClaims3.length !== 2) {
-    throw new Error("Line Manager Claims Insert failed");
-  }
-  const rejectedClaim2 = await lineManagerRole.rejectClaim(
-    rejectedClaimSubmit,
-    "Rejected for testing"
-  );
-  if (!rejectedClaim2) {
-    throw new Error("Line Manager Rejected Claim Insert failed");
-  }
-  console.log(
-    "Line Manager Rejected Claim Insert successful",
-    `id: ${rejectedClaim2.getId()}, amount: ${rejectedClaim2.getAmount()} status: ${rejectedClaim2.getStatus()}`
   );
 }
 
--- backend/db/utils/helpers.ts
+++ backend/db/utils/helpers.ts
@@ -4,20 +4,23 @@ import { Administrator } from "@/backend/employee/administrator";
 import { LineManager } from "@/backend/employee/lineManager";
 import { PayrollOfficer } from "@/backend/employee/payrollOfficer";
 import { defaultPassword, userDetails } from "./consts";
+import { Claim } from "@/backend/claims/claim";
 
 const create_temp = async (db: DatabaseManager): Promise<string> => {
   return "sdf";
 };
 
+// Create Employees
 const createEmployee = async (
   db: DatabaseManager,
-  user: User,
-  failMessage: string
+  user: User
 ): Promise<User> => {
   const employee = await db.addAccount(user, defaultPassword);
 
   if (!employee) {
-    throw new Error(failMessage);
+    throw new Error(
+      `Insert ${user.getEmployeeType()} failed for ${user.getFirstName()}`
+    );
   }
 
   return employee;
@@ -27,11 +30,7 @@ export const createGeneralEmployee = async (
   db: DatabaseManager
 ): Promise<User> => {
   const employeeDetails = userDetails.generalEmployee;
-  const emp = await createEmployee(
-    db,
-    employeeDetails,
-    "General Staff Insert failed"
-  );
+  const emp = await createEmployee(db, employeeDetails);
 
   console.log("General Staff Insert successful", `id: ${emp.getId()}`);
   return emp;
@@ -40,11 +39,7 @@ export const createGeneralEmployee = async (
 export const createConsultant = async (db: DatabaseManager): Promise<User> => {
   const employeeDetails = userDetails.consultant;
 
-  const emp = await createEmployee(
-    db,
-    employeeDetails,
-    "Consultant Insert failed"
-  );
+  const emp = await createEmployee(db, employeeDetails);
 
   console.log("Consultant Insert successful", `id: ${emp.getId()}`);
   return emp;
@@ -52,11 +47,7 @@ export const createConsultant = async (db: DatabaseManager): Promise<User> => {
 
 export const createPayroll = async (db: DatabaseManager): Promise<User> => {
   const employeeDetails = userDetails.payrollOfficer;
-  const emp = await createEmployee(
-    db,
-    employeeDetails,
-    "Payroll Officer Insert failed"
-  );
+  const emp = await createEmployee(db, employeeDetails);
 
   const payrollOfficerRole = emp.getEmployeeRole();
   if (!(payrollOfficerRole instanceof PayrollOfficer)) {
@@ -69,11 +60,7 @@ export const createPayroll = async (db: DatabaseManager): Promise<User> => {
 
 export const createAdmin = async (db: DatabaseManager): Promise<User> => {
   const employeeDetails = userDetails.admin;
-  const administrator = await createEmployee(
-    db,
-    employeeDetails,
-    "Administrator Insert failed"
-  );
+  const administrator = await createEmployee(db, employeeDetails);
   const administratorRole = administrator.getEmployeeRole();
 
   if (!(administratorRole instanceof Administrator)) {
@@ -89,11 +76,7 @@ export const createAdmin = async (db: DatabaseManager): Promise<User> => {
 
 export const createLineManager = async (db: DatabaseManager): Promise<User> => {
   const employeeDetails = userDetails.lineManager;
-  const emp = await createEmployee(
-    db,
-    employeeDetails,
-    "LineManager Insert failed"
-  );
+  const emp = await createEmployee(db, employeeDetails);
 
   const lineManagerRole = emp.getEmployeeRole();
 
@@ -104,3 +87,135 @@ export const createLineManager = async (db: DatabaseManager): Promise<User> => {
   console.log("LineManager Insert successful", `id: ${emp.getId()}`);
   return emp;
 };
+
+// Create Claims
+
+const updateClaim = async (
+  emp: User,
+  claim: Claim,
+  newValue: number
+): Promise<Claim> => {
+  const updatedClaim = await emp
+    .getEmployeeRole()
+    .updateClaimAmount(claim, 100);
+  if (!updatedClaim) {
+    throw new Error(`${emp.getEmployeeType()} Claim Update failed`);
+  }
+  console.log(
+    `${emp.getEmployeeType()} ${updatedClaim.getStatus()} Claim Update successful`,
+    `id: ${updatedClaim.getId()}, amount: ${updatedClaim.getAmount()}`
+  );
+
+  return updatedClaim;
+};
+
+const submitClaim = async (emp: User, claim: Claim): Promise<Claim> => {
+  const submittedClaim = await emp.getEmployeeRole().submitClaim(claim);
+  if (!submittedClaim) {
+    throw new Error(`G${emp.getEmployeeType()} Pending Claim Submit failed`);
+  }
+  console.log(
+    "General Staff Pending Claim Submit successful",
+    `id: ${submittedClaim.getId()}, amount: ${submittedClaim.getAmount()} status: ${submittedClaim.getStatus()}`
+  );
+
+  return submittedClaim;
+};
+
+const lineManagerApproveClaim = async (
+  lineManager: User,
+  claimToAccept: Claim
+): Promise<Claim> => {
+  const lineManagerRole = lineManager.getEmployeeRole() as LineManager;
+
+  const managersClaims = await lineManagerRole.getEmployeeSubmittedClaims();
+  if (managersClaims.length !== 2) {
+    throw new Error(
+      `Line Manager Claims Insert failed length: ${managersClaims.length}`
+    );
+  }
+  const acceptedClaim = await lineManagerRole.approveClaim(
+    claimToAccept
+  );
+  if (!acceptedClaim) {
+    throw new Error("Line Manager Accepted Claim Insert failed");
+  }
+  console.log(
+    "Line Manager Accepted Claim Insert successful",
+    `id: ${acceptedClaim.getId()}, amount: ${acceptedClaim.getAmount()} status: ${acceptedClaim.getStatus()}`
+  );
+
+  return claimToAccept;
+};
+
+const lineManagerRejectClaim = async (
+  lineManager: User,
+  claimToReject: Claim,
+  reason: string
+): Promise<Claim> => {
+  const lineManagerRole = lineManager.getEmployeeRole() as LineManager;
+
+  const managersClaims = await lineManagerRole.getEmployeeSubmittedClaims();
+  if (managersClaims.length !== 2) {
+    throw new Error("Line Manager Claims Insert failed");
+  }
+  const rejectedClaim2 = await lineManagerRole.rejectClaim(
+    claimToReject,
+    reason
+  );
+  if (!rejectedClaim2) {
+    throw new Error("Line Manager Rejected Claim Insert failed");
+  }
+  console.log(
+    "Line Manager Rejected Claim Insert successful",
+    `id: ${rejectedClaim2.getId()}, amount: ${rejectedClaim2.getAmount()} status: ${rejectedClaim2.getStatus()}`
+  );
+
+  return claimToReject;
+};
+
+export const createDraftClaim = async (emp: User): Promise<Claim> => {
+  const claim = await emp.getEmployeeRole().createDraftClaim();
+
+  if (!claim) {
+    throw new Error(`Claim Insert failed for ${emp.getEmployeeType()}`);
+  }
+
+  console.log(
+    `${emp.getEmployeeType()} Draft Claim Insert successful`,
+    `id: ${claim.getId()}`
+  );
+
+  return claim;
+};
+
+export const createPendingClaim = async (
+  emp: User,
+  updatedValue: number
+): Promise<Claim> => {
+  const draftClaim = await createDraftClaim(emp);
+  const updatedClaim = await updateClaim(emp, draftClaim, updatedValue);
+
+  // Submit claim
+  return await submitClaim(emp, updatedClaim);
+};
+
+export const createApprovedClaim = async (
+  emp: User,
+  updatedValue: number,
+  lineManager: User
+): Promise<Claim> => {
+  const claimToApprove = await createPendingClaim(emp, updatedValue);
+  return lineManagerApproveClaim(lineManager, claimToApprove);
+};
+
+export const createRejectedClaim = async (
+  emp: User,
+  updatedValue: number,
+  lineManager: User
+): Promise<Claim> => {
+  const claimToReject = await createPendingClaim(emp, updatedValue);
+  return lineManagerRejectClaim(lineManager, claimToReject, "Value requested too high, receipt says 50");
+};
+
+export const createReimbusedClaim = async () => {};
